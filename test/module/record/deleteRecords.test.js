40720acb7d221a33b452cb117bff63fe98ab58a7
vonhunhu <vonhunhu@yahoo.com>
2018-09-06 09:57:04 +0700

    Implement for deleting cases

--- test/module/record/deleteRecords.test.js
+++ test/module/record/deleteRecords.test.js
@@ -67,6 +67,35 @@ describe('deleteRecords function', () => {
     /**
     * Todo: implement more case
     */
+   describe('multiple delete', () => {
+    it('delete when specifying array of multiple ids', () => {
+      const data = {
+        app: 2,
+        ids: [1,3,2,4,5]
+      };
+      nock('https://' + common.DOMAIN)
+        .intercept('/k/v1/records.json', 'DELETE', (rqBody) => {
+          expect(rqBody.app).toEqual(data.app);
+          expect(rqBody.ids).toEqual(data.ids);
+          return true;
+        })
+        .matchHeader(common.PASSWORD_AUTH, (authHeader) => {
+          expect(authHeader).toBe(common.getPasswordAuth(common.USERNAME, common.PASSWORD));
+          return true;
+        })
+        .matchHeader('Content-Type', (type) => {
+          expect(type).toBe('application/json');
+          return true;
+        })
+        .reply(200, {});
+
+      const deleteRecordsResult = recordModule.deleteRecords(data.app, data.ids);
+      return deleteRecordsResult.then((rsp) => {
+        expect(rsp).toEqual({});
+      });
+    });
+  });
+
   });
 
   describe('error case', () => {
@@ -93,5 +122,66 @@ describe('deleteRecords function', () => {
     /**
     * Todo: implement another error case
     */
+    describe('invalid app ID', () => {
+      it('error will be displayed when using invalid app ID ', () => {
+        const data = {
+          app: -2,
+          ids: [1]
+        };
+        nock('https://' + common.DOMAIN)
+          .intercept('/k/v1/records.json', 'DELETE', (rqBody) => {
+            expect(rqBody.app).toEqual(data.app);
+            expect(rqBody.ids).toEqual(data.ids);
+            return true;
+          })
+          .reply(404, {'code': 'CB_VA01', 'id': '84Xn1q5RFbwN40k7K3ej', 'message': '入力内容が正しくありません。'});
+
+        const deleteRecordsResult = recordModule.deleteRecords(data.app, data.ids);
+        return deleteRecordsResult.catch((err) => {
+          expect(err).toBeInstanceOf(KintoneAPIException);
+        });
+      });
+    });
+
+    describe('Missing required field (app)', () => {
+      it(' error will be displayed when using method without app ID ', () => {
+        const data = {
+            ids: [1]
+        };
+        nock('https://' + common.DOMAIN)
+          .intercept('/k/v1/records.json', 'DELETE', (rqBody) => {
+           //s expect(rqBody.app).toEqual(data.app);
+            expect(rqBody.ids).toEqual(data.ids);
+            return true;
+          })
+          .reply(404, {'code': 'CB_IJ01', 'id': '84Xn1q5RFbwN40k7K3ej', 'message': '不正なJSON文字列です。'});
+
+        const deleteRecordsResult = recordModule.deleteRecords(data.app, data.ids);
+        return deleteRecordsResult.catch((err) => {
+          expect(err).toBeInstanceOf(KintoneAPIException);
+        });
+      });
+    });
+
+    describe('Missing required field (ids)', () => {
+      it(' error will be displayed when using method without ids ', () => {
+        const data = {
+            app: 1
+        };
+        nock('https://' + common.DOMAIN)
+          .intercept('/k/v1/records.json', 'DELETE', (rqBody) => {
+            expect(rqBody.app).toEqual(data.app);
+           // expect(rqBody.ids).toEqual(data.ids);
+            return true;
+          })
+          .reply(404, {'code': 'CB_VA01', 'id': '84Xn1q5RFbwN40k7K3ej', 'message': '入力内容が正しくありません。'});
+
+        const deleteRecordsResult = recordModule.deleteRecords(data.app, data.ids);
+        return deleteRecordsResult.catch((err) => {
+          expect(err).toBeInstanceOf(KintoneAPIException);
+        });
+      });
+    });
+
   });
 });
