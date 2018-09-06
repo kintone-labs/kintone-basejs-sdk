310f007552974331d23f85191a40b5ab9aa17f70
KhiemLuc <41050460+luckhiem@users.noreply.github.com>
2018-09-06 11:30:07 +0700

    update common file

--- test/common.js
+++ test/common.js
@@ -6,8 +6,30 @@ module.exports = {
   PASSWORD: 'your_password',
   PROXY_HOST: 'your_proxy',
   PROXY_PORT: '3128',
+  GUEST_SPACEID: 'your_guest_space_ID',
   getPasswordAuth: (userName, password) => {
     return Buffer.from(userName + ':' + password).toString('base64');
+  },
+  generateRecord: (number, inputRecord) => {
+    const items = [];
+    if (inputRecord.hasOwnProperty('id')) {
+        for (let i = 0; i < number; i++) {
+            items.push({ ...inputRecord })
+            items[i].id = i;
+        }
+    }
+    else if(typeof inputRecord === 'object'){
+        for (let i = 0; i < number; i++) {
+            items.push({ ...inputRecord })
+        }
+    }
+    else{
+        for (let i = 0; i < number; i++) {
+            items.push({ ...inputRecord })
+            items[i] = "user" + i;
+        }
+    }
+    return items;
   }
 };
 
