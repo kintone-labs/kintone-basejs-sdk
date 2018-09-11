60a40abe8391f4b5feb57d28a1aa476ff27987a0
vu-tran <v001132@cybozu.vn>
2018-09-11 09:57:19 +0700

    implemented unit test for function addComment()

--- test/module/record/addComment.test.js
+++ test/module/record/addComment.test.js
@@ -135,6 +135,32 @@ describe("addComment function", () => {
         expect(response).toMatchObject(expectedResult);
       });
     });
+
+    it("[RecordModule-252] should add a comment successfully when inputting string for appId", () => {
+      const data = {
+        app: 1,
+        record: 2,
+        comment: { text: "something goes here" }
+      };
+      const expectedResult = { id: "8" };
+      nock(URI)
+        .post(ROUTE, reqBody => {
+          expect(reqBody).toHaveProperty("app");
+          expect(reqBody).toHaveProperty("record");
+          expect(reqBody).toHaveProperty("comment");
+          return true;
+        })
+        .reply(200, expectedResult);
+
+      const actualResult = recordModule.addComment(
+        data.app,
+        data.record,
+        data.comment
+      );
+      return actualResult.then(response => {
+        expect(response).toMatchObject(expectedResult);
+      });
+    });
   });
 
   describe("error case", () => {
@@ -179,8 +205,202 @@ describe("addComment function", () => {
         });
       });
     });
-    /**
-     * Todo: implement more case
-     */
+
+    // it("[RecordModule-242] should return an error when specifying an unexisted user in mention", () => {
+    //   const data = {
+    //     app: 1,
+    //     record: 2,
+    //     comment: {
+    //       text: "something goes here",
+    //       mention: [
+    //         {
+    //           code: "un-existed",
+    //           type: "USER"
+    //         }
+    //       ]
+    //     }
+    //   };
+    //   const expectedResult = {
+    //     code: "CB_VA01",
+    //     id: "7oiYHOZd11fTpyvY00kG",
+    //     message: "Missing or invalid input.",
+    //     errors: {
+    //       "comment.text": {
+    //         messages: [
+    //           "Enter between 1 and 65,535 characters.",
+    //           "Required field."
+    //         ]
+    //       }
+    //     }
+    //   };
+    //   nock(URI)
+    //     .post(ROUTE, reqBody => {
+    //       expect(reqBody).toHaveProperty("comment");
+    //       expect(reqBody.comment).toHaveProperty("mention");
+    //       return true;
+    //     })
+    //     .reply(400, expectedResult);
+    //   const actualResult = recordModule.addComment(
+    //     data.app,
+    //     data.record,
+    //     data.comment
+    //   );
+    //   return actualResult.catch(err => {
+    //     expect(err).toBeInstanceOf(KintoneException);
+    //     expect(err.get()).toMatchObject(expectedResult);
+    //   });
+    // });
+
+    it("[RecordModule-246] should return an error when using invalid appId", () => {
+      const data = {
+        app: -1,
+        record: 2,
+        comment: { text: "something goes here" }
+      };
+      const expectedResult = {
+        code: "CB_VA01",
+        id: "R4E6puJFh6nDPXypT796",
+        message: "入力内容が正しくありません。",
+        errors: {
+          app: {
+            messages: ["最小でも1以上です。"]
+          }
+        }
+      };
+      nock(URI)
+        .post(ROUTE, reqBody => {
+          expect(reqBody.app).toBeLessThan(0);
+          return true;
+        })
+        .reply(400, expectedResult);
+      const actualResult = recordModule.addComment(
+        data.app,
+        data.record,
+        data.comment
+      );
+      return actualResult.catch(err => {
+        expect(err).toBeInstanceOf(KintoneException);
+        expect(err.get()).toMatchObject(expectedResult);
+      });
+    });
+    it("[RecordModule-247] should return an error when using invalid recordId", () => {
+      const data = {
+        app: 1,
+        record: -2,
+        comment: { text: "something goes here" }
+      };
+      const expectedResult = {
+        code: "CB_VA01",
+        id: "MDx5kAIOfK4AbeJssEYW",
+        message: "入力内容が正しくありません。",
+        errors: {
+          record: {
+            messages: ["最小でも1以上です。"]
+          }
+        }
+      };
+      nock(URI)
+        .post(ROUTE, reqBody => {
+          expect(reqBody.record).toBeLessThan(0);
+          return true;
+        })
+        .reply(400, expectedResult);
+      const actualResult = recordModule.addComment(
+        data.app,
+        data.record,
+        data.comment
+      );
+      return actualResult.catch(err => {
+        expect(err).toBeInstanceOf(KintoneException);
+        expect(err.get()).toMatchObject(expectedResult);
+      });
+    });
+    it("[RecordModule-248] should return an error when missing appId", () => {
+      const data = {
+        app: undefined,
+        record: 2,
+        comment: { text: "something goes here" }
+      };
+      const expectedResult = {
+        code: "CB_VA01",
+        id: "9JAS954ZZpOZk7PcZ3JS",
+        message: "入力内容が正しくありません。",
+        errors: {
+          app: {
+            messages: ["必須です。"]
+          }
+        }
+      };
+      nock(URI)
+        .post(ROUTE, reqBody => {
+          expect(reqBody).not.toHaveProperty("app");
+          return true;
+        })
+        .reply(400, expectedResult);
+
+      const actualResult = recordModule.addComment(
+        data.app,
+        data.record,
+        data.comment
+      );
+    });
+    it("[RecordModule-249] should return an error when missing recordId", () => {
+      const data = {
+        app: 1,
+        record: undefined,
+        comment: { text: "something goes here" }
+      };
+      const expectedResult = {
+        code: "CB_VA01",
+        id: "jxdkEErN6fBh5Uip6qik",
+        message: "入力内容が正しくありません。",
+        errors: {
+          record: {
+            messages: ["必須です。"]
+          }
+        }
+      };
+      nock(URI)
+        .post(ROUTE, reqBody => {
+          expect(reqBody).not.toHaveProperty("record");
+          return true;
+        })
+        .reply(400, expectedResult);
+
+      const actualResult = recordModule.addComment(
+        data.app,
+        data.record,
+        data.comment
+      );
+    });
+    it("[RecordModule-250] should return an error when missing comment", () => {
+      const data = {
+        app: 1,
+        record: undefined,
+        comment: undefined
+      };
+      const expectedResult = {
+        code: "CB_VA01",
+        id: "GCUZnHDYCC6bvKjOSgoB",
+        message: "入力内容が正しくありません。",
+        errors: {
+          comment: {
+            messages: ["必須です。"]
+          }
+        }
+      };
+      nock(URI)
+        .post(ROUTE, reqBody => {
+          expect(reqBody).not.toHaveProperty("comment");
+          return true;
+        })
+        .reply(400, expectedResult);
+
+      const actualResult = recordModule.addComment(
+        data.app,
+        data.record,
+        data.comment
+      );
+    });
   });
 });
