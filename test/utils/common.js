67ca1c73706e84a31e864d513439e0db4ce6b5c9
Trinh Hung Anh <anh-trinh@cybozu.vn>
2018-09-17 16:01:41 +0700

    Revert "Merge branch 'v0.1.6_unit_test_h' into v0.1.6_unit_test"
    This reverts commit fd494eb71fc140dbb8d96e1b43293581b063abb8, reversing
    changes made to be4ee6689afd7b3c809dca21ffdd414b361f24f6.

--- test/utils/common.js
+++ test/utils/common.js
@@ -6,9 +6,16 @@ module.exports = {
   PASSWORD: 'your_password',
   PROXY_HOST: 'your_proxy',
   PROXY_PORT: '3128',
-  GUEST_SPACEID: 1,
-  MAIN_PATH: '../../../src/main',
-
+  GUEST_SPACEID: 'your_guest_space_ID',
   getPasswordAuth: (userName, password) => {
     return Buffer.from(userName + ':' + password).toString('base64');
   },
+  generateRecord: (number, inputRecord) => {
+    const items = [];
+    for (let i = 0; i < number; i++) {
+      items.push(inputRecord);
+    }
+    return items;
+  }
+};
+
