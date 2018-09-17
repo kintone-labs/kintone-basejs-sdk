2fd5590ccc1787b3cb567ae9a96d6fb48bc29b81
KhiemLuc <41050460+luckhiem@users.noreply.github.com>
2018-09-17 14:01:26 +0700

    update common file

--- test/utils/common.js
+++ test/utils/common.js
@@ -2,8 +2,8 @@ module.exports = {
   PASSWORD_AUTH: 'X-Cybozu-Authorization',
   API_TOKEN: 'X-Cybozu-API-Token',
   DOMAIN: 'sample.cybozu.com',
-  USERNAME: 'cybozu',
-  PASSWORD: 'cybozu',
+  USERNAME: 'your_username',
+  PASSWORD: 'your_password',
   PROXY_HOST: 'your_proxy',
   PROXY_PORT: '3128',
   GUEST_SPACEID: 1,
@@ -11,4 +11,11 @@ module.exports = {
   getPasswordAuth: (userName, password) => {
     return Buffer.from(userName + ':' + password).toString('base64');
   },
+  generateRecord: (number, inputRecord) => {
+    const items = [];
+    for (let i = 0; i < number; i++) {
+      items.push(inputRecord);
+    }
+    return items;
+  }
 }
