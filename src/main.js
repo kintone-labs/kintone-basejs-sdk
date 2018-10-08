baf7c04f8319c5c3f5ca5a5b8ed2a2911ed62293
Cuc Ho <cuc-ho@cybozu.vn>
2018-10-08 16:30:37 +0700

    Common has not implement yet

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ module.exports = {
   Record: require('./module/record/Record'),
   BulkRequest: require('./module/bulkRequest/BulkRequest'),
   App: require('./module/app/App'),
-  Comment: require('./module/comment/Comment'),
   Connection: require('./connection/Connection'),
   Auth: require('./authentication/Auth'),
   File: require('./module/file/File'),
